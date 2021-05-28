ceb197d7fe3a81c889f20d5322b4342056494295
nitronium102 <nitronium@ewhain.net>
2021-05-28 22:42:19 +0900

    TDD - 테스트 코드 작성

---
+++ EFUB_Back/Week8/test-practice/src/test/java/com/efub/seminar/springboot/web/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.efub.seminar.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc; // MockMvc : web api test(get, post)
+
+    // 테스트 메소드
+    @Test
+    public void hello_return() throws Exception {
+        String hello = "hello";
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+}
