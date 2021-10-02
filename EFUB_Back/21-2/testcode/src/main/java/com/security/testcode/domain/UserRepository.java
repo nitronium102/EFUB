ceb09e053627a021ab0d37b62cd875ac343aea67
Minji Kang <nitronium@ewhain.net>
2021-10-02 11:41:37 +0900

    폴더 구조 변경

--- EFUB_Back/21-2/testcode/src/main/java/com/security/testcode/domain/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.security.testcode.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
